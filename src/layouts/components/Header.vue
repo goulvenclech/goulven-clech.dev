775059877c4048a20e813420eef2692cdb7c9e19
Goulven Nev <nevilain@gmail.com>
2020-10-24 22:31:44 +0200

    Menu mobile temporaire

--- src/layouts/components/Header.vue
+++ src/layouts/components/Header.vue
@@ -5,27 +5,63 @@
                 Goulven CLEC'H
             </a>
         </h1>
-        <ul class="text-base inline-block float-right font-body">
-            <li><a href="/resume">A propos</a></li>
-            <li><a href="/folio">Portfolio</a></li>
-            <li><a href="/blog">Blog</a></li>
-            <li><a href="https://github.com/GoulvenC" target="_blank">Github</a></li>
-        </ul>
+        <!-- Menu Desktop -->
+        <nav class="float-none md:float-right">
+            <ul class="text-base font-body">
+                <li><a href="/resume">A propos</a></li>
+                <li><a href="/folio">Portfolio</a></li>
+                <li><a href="/blog">Blog</a></li>
+                <li><a href="https://github.com/GoulvenC" target="_blank">Github</a></li>
+            </ul>
+        </nav>
+        <!-- Menu mobile Switcher WIP
+        <button class="lg:hidden" @click.native="toggleMobileMenu()">
+            <g-image src="~/assets/menu.svg"
+                     immediate="true"
+                     quality="100"
+            />
+        </button> -->
     </header>
 </template>
 
 <style lang="postcss">
     header {
-        line-height: 50px;
+        line-height: 40px;
 
         & li {
-            @apply inline-block ml-4;
+            @apply inline-block align-top mr-4 pb-2;
 
-            line-height: 50px;
+            line-height: 30px;
 
             & a {
                 @apply border-none;
             }
+        }
+    }
+
+    button {
+        touch-action: manipulation;
+        float: right;
+
+        & img {
+            @apply inline-block;
+
+            height: 30px;
+            width: 30px;
+            margin: 10px;
+        }
+
+        &:hover {
+            @apply bg-area cursor-pointer;
+
+            border-radius: 24px;
+        }
+    }
+
+    @screen md {
+        header, header li {
+            line-height: 50px;
+            padding-bottom: 0;
         }
     }
 </style>
