9b78d762ad304663be32810614957e4b507547a9
Goulven Clec'h <goulven.clech@protonmail.com>
2023-02-22 08:37:19 +0100

    feat(darkmode): now supported 🌚

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,26 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  // See -> https://tailwindcss.com/docs/dark-mode
+  darkMode: "class",
+  // See -> https://docs.astro.build/en/guides/integrations-guide/tailwind/
+  content: ["./src/**/*.{astro,mdx,ts,tsx}"],
   theme: {
     extend: {
+      // See -> https://tailwindcss.com/docs/customizing-colors
       colors: {
+        // Document default body color
         body: {
           light: "#FDF0ED",
           dark: "#1C1E26",
         },
-        body2: {
+        // Alt body color, for example when a div is hovered
+        highlight: {
           light: "#FADAD1",
+          dark: "#2E303E",
         },
+        // Mainly used by text and icons
         base: {
           light: "#1C1E26",
           dark: "#FDF0ED",
         },
-        primary: {
-          light: "#EC6A88",
-          dark: "#E95678",
-        },
+        // Mainly used when hovering buttons or links
+        primary: "#EC6A88",
       },
     },
   },
+  // Mainly used for src/components/Card.astro
+  // see ->  https://github.com/tailwindlabs/tailwindcss-line-clamp
   plugins: [require("@tailwindcss/line-clamp")],
 }
