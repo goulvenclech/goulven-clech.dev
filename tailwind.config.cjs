9591ed99a17b10b05357dbee8d86929fa38fc0c1
Goulven Clec'h <goulven.clech@protonmail.com>
2023-02-22 15:31:03 +0100

    fix(misc): responsive & button zone & format & 404

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,26 +5,36 @@ module.exports = {
   // See -> https://docs.astro.build/en/guides/integrations-guide/tailwind/
   content: ["./src/**/*.{astro,mdx,ts,tsx}"],
   theme: {
+    /**
+     * Blog custom colors 🎨
+     * See -> https://tailwindcss.com/docs/customizing-colors
+     */
+    colors: {
+      // Document default body color
+      body: {
+        light: "#FDF0ED",
+        dark: "#1C1E26",
+      },
+      // Alt body color, for example when a div is hovered
+      highlight: {
+        light: "#FADAD1",
+        dark: "#2E303E",
+      },
+      // Mainly used by text and icons
+      base: {
+        light: "#1C1E26",
+        dark: "#FDF0ED",
+      },
+      // Mainly used when hovering buttons or links
+      primary: "#EC6A88",
+    },
     extend: {
-      // See -> https://tailwindcss.com/docs/customizing-colors
-      colors: {
-        // Document default body color
-        body: {
-          light: "#FDF0ED",
-          dark: "#1C1E26",
-        },
-        // Alt body color, for example when a div is hovered
-        highlight: {
-          light: "#FADAD1",
-          dark: "#2E303E",
-        },
-        // Mainly used by text and icons
-        base: {
-          light: "#1C1E26",
-          dark: "#FDF0ED",
-        },
-        // Mainly used when hovering buttons or links
-        primary: "#EC6A88",
+      /**
+       * Cause Tailwind just offer 2 & 4 for some reason 🤷‍♂️
+       * See -> https://tailwindcss.com/docs/text-underline-offset
+       */
+      textUnderlineOffset: {
+        3: "3px",
       },
     },
   },
