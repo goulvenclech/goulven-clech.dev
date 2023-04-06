871b4a347c17bd147226461a2e7e21ba5cb6a0a2
Goulven Clec'h <goulven.clech@protonmail.com>
2023-04-06 08:42:14 +0200

    upgrade(images): astro 2.2

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,17 +1,18 @@
 // 1. Import utilities from `astro:content`
-import { image, z, defineCollection } from "astro:content"
+import { z, defineCollection } from "astro:content"
 // 2. Define your collection(s)
 const blogCollection = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    date: z.date(),
-    tags: z.array(z.string()),
-    abstract: z.string(),
-    image: image(),
-    image_alt: z.string(),
-    image_caption: z.string(),
-    draft: z.boolean().default(false),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      date: z.date(),
+      tags: z.array(z.string()),
+      abstract: z.string(),
+      image: image(),
+      image_alt: z.string(),
+      image_caption: z.string(),
+      draft: z.boolean().default(false),
+    }),
 })
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
