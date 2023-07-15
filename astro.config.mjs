80accefbcf6ef8a8d09df5a7e23ca1d6bb3ed914
Goulven Clec'h <goulven.clech@protonmail.com>
2023-07-15 18:03:26 +0200

    deps(astro-expressive-code): replace FigCode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,34 @@
 import { defineConfig } from "astro/config"
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind"
-
-// https://astro.build/config
 import mdx from "@astrojs/mdx"
+import { astroExpressiveCode } from "astro-expressive-code"
+import expressiveCode from "astro-expressive-code"
+
+//  https://www.npmjs.com/package/astro-expressive-code
+/** @type {import('astro-expressive-code').AstroExpressiveCodeOptions} */
+const astroExpressiveCodeOptions = {
+  styleOverrides: {
+    borderRadius: "0.5rem",
+    codeBackground: "#2E303E",
+  },
+  frames: {
+    styleOverrides: {
+      terminalBackground: "#2E303E",
+    },
+  },
+}
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
-  markdown: { shikiConfig: { theme: "css-variables" } },
-  experimental: { assets: true },
+  integrations: [tailwind(), astroExpressiveCode(astroExpressiveCodeOptions), mdx()],
+  markdown: {
+    shikiConfig: {
+      theme: "css-variables",
+    },
+  },
+  experimental: {
+    assets: true,
+  },
   image: {
     service: {
       entrypoint: "./src/imageService.ts",
